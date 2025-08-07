2f581216a10d373d0b795dcbbd7de0db43e267f1
lvyf <lvyf1@foxmail.com>
2025-08-08 00:16:59 +0800

    feat(blog): 新增博客文章创建和草稿保存功能
    - 新增创建博客文章和保存草稿的接口
    - 实现博客文章状态的自动设置
    - 优化博客标签的保存逻辑
    - 更新响应代码和消息
    - 添加系统常量的定义

---
+++ cloud-common/src/main/java/org/luckycloud/constant/BlogConstant.java
@@ -0,0 +1,15 @@
+package org.luckycloud.constant;
+
+/**
+ * @author lvyf
+ * @description:
+ * @date 2025/8/7
+ */
+public class BlogConstant {
+
+    public static class BlogStatus {
+        public static final String DRAFT = "BS00"; // 草稿
+        public static final String PUBLIC = "BS01"; // 公开
+        public static final String PRIVATE = "BS02"; // 私有
+    }
+}
